b558bf24a6bb3d38779a4a06e919bf1610e49c95
Vicheak <suonvicheak991@gmail.com>
2024-07-26 16:27:08 +0700

    update

---
+++ k8s_playbook/shellscript/apply_argocd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+YAML_CONTENT=$(cat << EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: reactjs-application
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: https://github.com/Vicheak/k8s_orchestration.git
+    targetRevision: main
+    path: argocd/react
+  destination:
+    namespace: default
+    server: https://kubernetes.default.svc
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - Validate=false
+EOF
+)
+
+TMP_FILE=$(mktemp)
+echo "$YAML_CONTENT" > "$TMP_FILE"
+
+kubectl apply -f "$TMP_FILE"
+
+rm "$TMP_FILE"
